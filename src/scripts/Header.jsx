b51c0c768f546e21660c0f073430b8331418a429
MR-kzk <82753724+MR-kzk@users.noreply.github.com>
2021-08-28 22:23:07 +0900

    feature: ヘッダー作成

---
+++ src/scripts/Header.jsx
@@ -0,0 +1,13 @@
+import '../assets/header.scss';
+
+function Header() {
+  return (
+    <header className="header-wrapper">
+      <div className="header__contents">
+        <h1>TalkTheme</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
